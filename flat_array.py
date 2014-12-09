acf3a5b890d5995161a2df41220530139cd2aff2
Denis Karanja <deebeat@students.uonbi.ac.ke>
2014-12-09 20:41:23 +0300

    Made a recurssive method to lookup lists in depth

--- flat_array.py
+++ flat_array.py
@@ -1,3 +1,22 @@
-array = [1, 2, 3, 4, [5, 6, 7, 8], 9, 10, 11, [12, 13, 14, 14, [16, 17, 18], 19], 20, 21]
+#!/usr/bin/python
+list_one = list_two = []
+array = [1, 2, 3, 4, [5, ["Kenya",["Nairobi",["Buruburu", ["Phase1", "Phase2"], "Lavington"], "Mombasa", "Kisumu"], "Uganda", ["Kampala", "Kigali"], "Tanzania", ["Dar", "Moshi", "Moro"]], 6, 7], [8, 9], 10, [11], "Abby", ["Moses"], ["Joseph"]]
 
-print array
+def flatten_list(my_list):
+  if type(my_list) is not list:
+    return "Function 'flatten_list' expects a list. '{}' given ".format(type(my_list))
+  elif type(my_list) is list:
+    #check if list is null
+    if len(my_list) != 0:
+      for i, num_one in enumerate(my_list):
+        #check if elements inside list is a list
+        if type(num_one) is list:
+          flatten_list(num_one)
+        else:
+          list_one.append(num_one)
+    else: return "List passed is empty."
+
+  return list_one
+
+print flatten_list(array)
+
